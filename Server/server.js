35429ccc4cb64cb24bbd47486e92bc90ccfce4ad
Pothapu <S525093@mail.nwmissouri.edu>
2016-11-10 12:06:15 -0600

    validations done for new collection Users

--- Server/server.js
+++ Server/server.js
@@ -4,28 +4,55 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo' , {
+// var Todo = mongoose.model('Todo' , {
+//
+// text:{
+// type: String
+// },
+// completed: {
+// type: Boolean
+// },
+// completedAt: {
+//   type:Number
+// }
+// });
+//
+// var otherTodo = new Todo ({
+//   text: 'Dinner Cooked' ,
+//   completed : true ,
+//   completedAt : 12345
+//
+// });
+//
+//
+// otherTodo.save().then((docs) =>{
+//
+//   console.log('hehe' ,docs);
+//
+// },
+//
+//  (e) => {
+//    console.log('error occured' , e);
+//  }
+// );
 
-text:{
-type: String
-},
-completed: {
-type: Boolean
-},
-completedAt: {
-  type:Number
-}
+
+var User  = mongoose.model('User' , {
+  email : {
+    required : true,
+    trim : true,
+    type: String,
+    minlength : 1
+  }
 });
 
-var newTodo = new Todo ({
-  text: 'Cook dinner'
-});
-newTodo.save().then((doc) =>{
-  console.log('document saved' ,doc);
+var NewUser = new User( {
+  email : ' lokesh@gmail.com '
+})
 
+NewUser.save().then ( (Userdoc) => {
+console.log("New  user object created" , Userdoc);
 },
-
- (e) => {
-   console.log('error occured' , e);
- }
-);
+(e) => {
+console.log("Error caught",e);
+});
